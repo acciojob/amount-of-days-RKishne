4b0a6425bc2330c629041eaa4b520bb33303f0dc
Rahul Kishne <kishnerahul123@gmail.com>
2023-12-11 14:44:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+const year=parseInt(prompt("Enter any year"));
+
+if(year%100==0){
+	if(year%400==0){
+		alert(366);
+	}
+	else{
+		alert(365);
+	}
+}
+if(year%4==0){
+	alert(366);
+}
+alert(365);
